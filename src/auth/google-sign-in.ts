4646001b2b849c037b412ddb860873746a2fcdca
Charif Mahmoudi <charif.mahmoudi.us@gmail.com>
2026-09-09 11:22:59 -0400

    ci: allow time for sequential android e2e

--- src/auth/google-sign-in.ts
+++
@@ -1,9 +0,0 @@
-export const isGoogleSignInAvailable = false;
-
-export async function getGoogleIdToken(): Promise<string | null> {
-  return null;
-}
-
-export async function signOutGoogle(): Promise<void> {
-  return undefined;
-}
